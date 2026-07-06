0cf43338bbd8baa3cc88c6365a2aa8d294324f6c
rohithh55 <rohiithh5@gmail.com>
2026-07-06 23:25:30 +0530

    Create main.tf

---
+++ modules/s3/main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
